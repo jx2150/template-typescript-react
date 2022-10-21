02c114e9fb6370c7bd43d5979962000a65b84f94
Jack Bishop <jack.bishop1@gmail.com>
2022-10-21 14:07:53 +0000

    initial spacex files

---
+++ codegen.ts
@@ -0,0 +1,23 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "https://spacexdata.herokuapp.com/graphql",
+  documents: "./src/**/*.{ts,tsx}",
+  generates: {
+    "src/generated/graphql.tsx": {
+      preset: "client",
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+      config: {
+        withHooks: true
+      }
+    }
+  }
+};
+
+export default config;
